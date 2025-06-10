d2ab50fac23966a3cfd5a8e9b46130b998517da7
Aditya YV <adityayv802@gmail.com>
2025-06-10 13:52:06 +0530

    updated topKfrequent to use buckets logic

--- topKfrequent.cpp
+++ topKfrequent.cpp
@@ -18,51 +18,28 @@ public:
             else{
                 um[num] = um[num] + 1;
             }
-            
-            if (toRet.size() < k){
-                
-                if (um[num]!=1){
-                    int inde =findElement(toRet, num);
-                    if (inde!=-1){
-                        toRet.erase(inde + toRet.begin());
-                    }
-                    else{
-                        toRet.erase(toRet.begin());
-                    }
-                    toRet.push_back(num);
-                }
-                else{ 
-                 toRet.push_back(num);
+        }
+        int n = nums.size();
+        vector<vector<int>> buckets(n+1);
+        
+        for (unordered_map<int,int>::iterator i = um.begin(); i!=um.end();i++){
+            int num=i->first;
+            int count=i->second;
+
+            buckets[count].push_back(num);
+        }
+
+        for (int i = buckets.size()-1; i >= 0; i--){
+            for (int j = buckets[i].size()-1; j >= 0; j--){
+                toRet.push_back(buckets[i][j]);
+                if (toRet.size() == k){
+                    break;
                 }
             }
-            else{
-                int inde = findElement(toRet, num);
-                if (inde != -1){
-                    for (int i = inde; i < toRet.size()-1;i++){
-                        if (um[toRet[i]] <= um[toRet[i+1]]){
-                            break;
-                        }
-                        int t = toRet[i];
-                        toRet[i] = toRet[i+1];
-                        toRet[i+1] = t;
-                    }
-                    continue;
-                }
-                if (um[num] > um[toRet[0]]){
-                    toRet.erase(toRet.begin());
-                    toRet.push_back(num);
-                }
+            if (toRet.size() == k){
+                break;
             }
         }
         return toRet;
-    }
-private:
-    int findElement(vector<int>& nums, int n){
-        for (int i = 0; i < nums.size(); i++){
-            if (nums[i] == n){
-                return i;
-            }
-        }
-        return -1;
     }
 };
